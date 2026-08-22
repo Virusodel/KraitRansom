8686365ecbe01a30e030a0fc713604f6d2b8c7f9
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-22 14:37:28 +0300

    Delete scr directory

--- scr/utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-#include <cstdint>
-
-std::vector<uint8_t> StringToBytes(const std::string& str);
-std::string BytesToString(const std::vector<uint8_t>& bytes);
