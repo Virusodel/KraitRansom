fadfdf812d7f3bad0a1da0254f5ed1722eb4b994
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-22 13:33:31 +0300

    Update utils.h

--- scr/utils.h
+++ scr/utils.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <cstdint>
 
 std::vector<uint8_t> StringToBytes(const std::string& str);
 std::string BytesToString(const std::vector<uint8_t>& bytes);
